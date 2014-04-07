e27882125045f8316cab1c0f99739ddb0d5bd4e5
thlebowicz <thomashlebowicz1@gmail.com>
2014-04-07 18:31:15 -0400

    Completed DoubleLL

--- 11-Double Linked List/DoubleLL.java
+++ 11-Double Linked List/DoubleLL.java
@@ -1,76 +1,86 @@
-//Thomas Hlebowicz, Partner: Malcolm Weils
-//Work in progress for now, still need to fix toString() method
-public class DoubleLL<E> { 
-    private class Node<E> {
+//Thomas Hlebowicz and Malcolm Wells
+
+public class DoubleLL<E>{
+    private class Node<E>{
 	E data;
 	Node<E> next,prev;
-	public Node(E d) {
+	public Node(E d){
 	    this.data = d;
 	}
-	public String toString() {
-	    return "" + data;
+	public String toString(){
+	    return ""+data;
 	}
-	public void setData(E d) { data = d; }
-	public E getData() { return data; }
-	
-	public void setNext(Node<E> n) { next = n; }
-	public Node<E> getNext() { return next; }
-	
-	public void setPrev(Node<E> n) { prev = n; }
-	public Node<E> getPrev() { return prev; }
+	public void setData(E d){data=d;}
+	public E getData(){return data;}
+	public void setNext(Node<E> n){next=n;}
+	public Node<E> getNext(){return next;}
+	public void setPrev(Node<E> n){prev=n;}
+	public Node<E> getPrev(){return prev;}
     }
     private Node<E> current;
-    
-    public void insert(E d) {
+    public void insert(E d){
 	Node<E> n = new Node<E>(d);
-	if (current==null) {
+	if(current == null){
 	    current = n;
-	}
-	else { 
-	    n.next = current;
+	    n.setNext(n);
+	    n.setPrev(n);
+	} else {
+	    current.prev.setNext(n);
 	    n.prev = current.prev;
-	    if ( current.prev != null )
-		current.prev.setNext(n);
+	    n.next = current;
 	    current.prev = n;
 	    current = n;
 	}
     }
-    
-    public E getCurrent() { 
+    public E getCurrent(){
 	return current.getData();
     }
-    
-    public void forward() { 
-	if (current.getNext() != null)
+    public void forward(){
+	if(current.getNext() != null)
 	    current = current.getNext();
     }
-    
-    public void back() {
-	if (current.getPrev() != null) 
+    public void back(){
+	if(current.getPrev() != null)
 	    current = current.getPrev();
     }
-
-    public String toString() {
-	if (current == null) 
+    public String toString(){
+	if(current == null){
 	    return "";
-	int ctr = 0;
-	while (current.getPrev() != null) { 
-	    current = current.getPrev();
-	    ctr++;
 	}
-	Node<E> tmp = current;
-	String s = "";
-	while ( tmp != null ) {
-	    s = s +tmp.getData()+" ";
+	Node<E> tmp = current.getNext();
+	String s = current.getData()+"";
+	while(tmp != current){
+	    s += " "+tmp.getData();
 	    tmp = tmp.getNext();
-	}
-	for ( int n = 0; n < ctr; n++ ) {
-	    current=current.getNext();
 	}
 	return s;
     }
-
-    public static void main( String[] args) {
+    public E delete(){
+	E x = null;
+	if(current.getNext() == current){
+	    x = current.data;
+	    current = null;
+	} else {
+	    current.prev.setNext(current.next);
+	    current.next.setPrev(current.prev);
+	    x = current.data;
+	    current = current.next;
+	}
+	return x;
+    }
+    public int find(E val){
+	Node<E> tmp = current.prev;
+	int i = 0;
+	while(current != tmp){
+	    if(current.data == val){
+		return i;
+	    }
+	    current = current.next;
+	    i++;
+	}
+	return -1;
+    }
+    public static void main(String[]args){
 	DoubleLL<String> L = new DoubleLL<String>();
 	System.out.println(L);
 	L.insert("hello");
@@ -79,13 +89,16 @@ public class DoubleLL<E> {
 	System.out.println(L);
 	L.insert("three");
 	System.out.println(L);
-	// First test up to here
+	//First test up to here
 	System.out.println(L.getCurrent());
 	L.forward();
 	System.out.println(L.getCurrent());
 	L.insert("inserted");
-	System.out.println(L);
-	// then test again to here
-				       
+	System.out.println(L); 
+	//then test to here
+	L.delete();
+	System.out.println(L); 
+	System.out.println(L.find("hello"));
+	
     }
 }
